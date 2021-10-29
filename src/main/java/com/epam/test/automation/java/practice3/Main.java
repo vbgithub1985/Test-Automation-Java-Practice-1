53317fe6ff2bd0f3079f93ccd3417a0b5362a0cc
Viktor <vbgithub1985@gmail.com>
2021-10-29 09:39:21 +0300

    refactoring main. add result array into the tasks

--- src/main/java/com/epam/test/automation/java/practice3/Main.java
+++ src/main/java/com/epam/test/automation/java/practice3/Main.java
@@ -8,26 +8,37 @@ public class Main {
 
     }
 
+    public static void main(String[] args) {
+        int[][] arr = {{3,4,6,2},{1,6,8,99},{5,66,77,5},{44,55,66,99}};
+        printMatrix(arr);
+        printMatrix(task3(arr));
+        printMatrix(arr);
+    }
+
     /**
      * <summary>
      * Implement code according to description of task 1.
      * </summary>
      */
     public static int[] task1(int[] array) {
+        int[] arrResult = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arrResult[i] = array[i];
+        }
         int iFirst = 0;
-        int iLast = array.length-1;
+        int iLast = arrResult.length-1;
         while (iFirst < iLast){
-            int first = array[iFirst];
-            int last = array[iLast];
+            int first = arrResult[iFirst];
+            int last = arrResult[iLast];
             if (first % 2 ==0 && last % 2 ==0){
-                int temp = array[iFirst];
-                array[iFirst] = array[iLast];
-                array[iLast] = temp;
+                int temp = arrResult[iFirst];
+                arrResult[iFirst] = arrResult[iLast];
+                arrResult[iLast] = temp;
             }
             iFirst++;
             iLast--;
         }
-        return array;
+        return arrResult;
     }
 
     /**
@@ -61,16 +72,22 @@ public class Main {
      * </summary>
      */
     public static int[][] task3(int[][] matrix) {
+        int[][] arrResult = new int[matrix.length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
+                arrResult[i][j] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < arrResult.length; i++) {
+            for (int j = 0; j < arrResult.length; j++) {
                 if (i<j) {
-                    matrix[i][j] = 1;
+                    arrResult[i][j] = 1;
                 }else if (i>j){
-                    matrix[i][j] = 0;
+                    arrResult[i][j] = 0;
                 }
             }
         }
-        return matrix;
+        return arrResult;
     }
 
     public static void printMatrix(int[][] matrix){
