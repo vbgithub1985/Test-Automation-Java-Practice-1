5e9c269c20f64fc483fa7d8ee489cc31682a48e8
Viktor Brechko <vbrobocop1985@gmail.com>
2021-11-14 22:38:34 +0200

    change  practice 6

--- src/main/java/com/epam/test/automation/java/practice6/Main.java
+++ src/main/java/com/epam/test/automation/java/practice6/Main.java
@@ -6,13 +6,15 @@ public class Main {
         Employee em2 = new SalesPerson("Petrov", 200000, 120 );
         Employee em3 = new SalesPerson("Sidorov", 150000, 70 );
         Employee em4 = new SalesPerson("Panacotov", 140000, 220 );
-        Employee m1 = new Manager("Ivanov", 1000000, 100);
-        Employee m2 = new Manager("Petrov", 2000000, 200);
-        Employee m3 = new Manager("Ivanov", 3000000, 500);
+        Employee m1 = new Manager("Ivanov manager", 1000000, 100);
+        Employee m2 = new Manager("Petrov manager", 2000000, 200);
+        Employee m3 = new Manager("Ivanov manager", 3000000, 500);
         Company company = new Company(em1, em2, em3, em4, m1, m2, m3);
         System.out.println(company.toString());
         company.giveEverybodyBonus(2000);
         System.out.println(company.toString());
+        System.out.println(company.TotalToPay());
+        System.out.println(company.nameMaxSalary());
 
 
     }
